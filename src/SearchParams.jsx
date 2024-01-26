6bae660d2577db959cd4ae6b32db58d928de8843
l3vy1337 <jakubik.levente04@gmail.com>
2024-01-26 15:05:05 +0100

    Add errorboundaries, modals and context

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
 import fetchSearch from "./fetchSearch";
 import Pet from "./Pet";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -15,6 +16,7 @@ export default function SearchParams() {
     });
     const [animal, setAnimal] = useState("");
     const [breeds] = useBreedList(animal);
+    const [adoptedPet] = useContext(AdoptedPetContext);
 
     const results = useQuery(["search", requestParams], fetchSearch);
     const pets = results?.data?.pets ?? [];
@@ -31,6 +33,13 @@ export default function SearchParams() {
                 };
                 setRequestParams(obj);
             }}>
+                {
+                    adoptedPet ? (
+                        <div className="pet image-container">
+                            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+                        </div>
+                    ) : null
+                }
                 <label htmlFor="location">
                     <input 
                             name="location"
